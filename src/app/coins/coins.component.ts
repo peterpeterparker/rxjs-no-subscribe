f12428c3443c9187b8a9fad12f519be5e856820d
peterpeterparker <david.dalbusco@outlook.com>
2021-09-10 12:26:52 +0200

    feat: subscribe in component

--- src/app/coins/coins.component.ts
+++ src/app/coins/coins.component.ts
@@ -1,16 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CoinsService } from '../coins.service';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-coins',
   templateUrl: './coins.component.html',
   styleUrls: ['./coins.component.css']
 })
-export class CoinsComponent implements OnInit {
+export class CoinsComponent implements OnInit, OnDestroy {
   constructor(private readonly coinsService: CoinsService) {}
 
+  private destroy$: Subject<void> = new Subject<void>();
+
   ngOnInit(): void {
-    this.coinsService.list();
+    this.coinsService
+      .list()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(() => {});
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 
   get coins() {
